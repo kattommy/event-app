5ec3ec40cadb29d3917420aef3519079704998c4
Jiohinnin <janmac98@wp.pl>
2021-10-14 19:00:02 +0200

    Added User entity and repository

--- src/main/java/com/project/eventapp/model/User.java
+++ src/main/java/com/project/eventapp/model/User.java
@@ -1,32 +1,22 @@
 package com.project.eventapp.model;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Data
 @Entity
-@Getter
-@Setter
+
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
-
-    @Column(length = 30)
-    public String firstName;
-
-    @Column(length = 30)
-    public String lastName;
-
-    @Column(length = 50)
-    public String email;
-
-    @Column(length = 30)
+    public String username;
     public String password;
-
-    //TODO relacja z eventami, komentarzami
+    public String email;
 
 }
