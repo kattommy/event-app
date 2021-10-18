f2e156051ea88b5b3aca83876f0cfbbaab2adc7f
Jiohinnin <janmac98@wp.pl>
2021-10-18 18:54:08 +0200

    added addUser form, users template fixed

---
+++ src/main/java/com/project/eventapp/service/CommentService.java
@@ -0,0 +1,15 @@
+package com.project.eventapp.service;
+
+import com.project.eventapp.repository.CommentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+
+    //TODO add something here
+}
