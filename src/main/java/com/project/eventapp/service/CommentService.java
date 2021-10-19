ae0acbef96978ea7f971e243609e822582484158
SzejkerPM <ghaawk@wp.pl>
2021-10-19 19:01:39 +0200

    Comment (created service and replaced repository in controller)

--- src/main/java/com/project/eventapp/service/CommentService.java
+++ src/main/java/com/project/eventapp/service/CommentService.java
@@ -1,8 +1,11 @@
 package com.project.eventapp.service;
 
+import com.project.eventapp.model.Comment;
 import com.project.eventapp.repository.CommentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -11,5 +14,19 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
 
-    //TODO add something here
+    public List<Comment> findAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Comment findCommentById(Long id) {
+        return commentRepository.findById(id).orElse(null);
+    }
+
+    public void saveComment(Comment commentToSave) {
+        commentRepository.save(commentToSave);
+    }
+
+    public void deleteCommentById(Long id) {
+        commentRepository.deleteById(id);
+    }
 }
