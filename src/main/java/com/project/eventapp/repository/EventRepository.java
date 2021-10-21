c1040883a736f453353f0af8b43ab95ae39d9d97
kattommy <katarzynaa.nowak@wp.pl>
2021-10-21 18:40:58 +0200

    add methods for EventRepository and CommentRepository

--- src/main/java/com/project/eventapp/repository/EventRepository.java
+++ src/main/java/com/project/eventapp/repository/EventRepository.java
@@ -4,6 +4,11 @@ import com.project.eventapp.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+
+    List<Event> findAllByUser_Id(Long id);
 }
