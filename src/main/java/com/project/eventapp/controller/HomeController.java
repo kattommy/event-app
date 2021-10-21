22051ff876fd75b9c25d6d15840ac13dac41a043
Pawel Parcz <pawelparcz@gmail.com>
2021-10-21 18:02:54 +0200

    Upiększanie widoku index

---
+++ src/main/java/com/project/eventapp/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.project.eventapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping( "/index")
+public class HomeController {
+    @GetMapping
+    public String getView(){
+        return "index";
+    }
+}
