561e569be939d6a6a8546103b712b5fab6ee79f7
Jiohinnin <janmac98@wp.pl>
2021-10-25 21:02:22 +0200

    spring security

--- src/main/java/com/project/eventapp/service/UserService.java
+++ src/main/java/com/project/eventapp/service/UserService.java
@@ -3,30 +3,44 @@ package com.project.eventapp.service;
 import com.project.eventapp.model.User;
 import com.project.eventapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userRepository.findAll();
     }
 
-    public User findUserById(Long id){
+    public User findUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
-
-    public void saveUser(User userToSave){
+    //TODO change name to updateUser()
+    public void saveUser(User userToSave) {
         userRepository.save(userToSave);
     }
 
-    public void deleteUserById(Long id){
+    public void deleteUserById(Long id) {
         userRepository.deleteById(id);
     }
 
+    public void createUser(User user){
+        if(user.getId() != null){
+            throw new IllegalArgumentException("Użytkownik o podanym ID już istnieje !!!");
+        }
+        saveUser(user);
+    }
+
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Brak użytkownika o email: " + email));
+    }
 }
