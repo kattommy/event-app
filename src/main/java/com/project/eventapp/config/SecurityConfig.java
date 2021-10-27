6cf721e00ac9fe6ae7a3191db675f73667c10ed9
Pawel Parcz <pawelparcz@gmail.com>
2021-10-27 17:29:17 +0200

    poprawiłem widok logowania

--- src/main/java/com/project/eventapp/config/SecurityConfig.java
+++ src/main/java/com/project/eventapp/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.formLogin()
-               .loginPage("/login")
+               .loginPage("/")
                .and()
                .logout()
                .logoutSuccessUrl("/")
