3e2079f7301f73bac1ae66efb9c1378836aa58ad
Pawel Parcz <pawelparcz@gmail.com>
2021-11-08 10:52:47 +0100

    validacja w user

--- src/main/java/com/project/eventapp/controllerAdvice/ErrorHandlerControllerAdvice.java
+++ src/main/java/com/project/eventapp/controllerAdvice/ErrorHandlerControllerAdvice.java
@@ -1,13 +1,12 @@
 package com.project.eventapp.controllerAdvice;
 
 
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ErrorHandlerControllerAdvice {
-    @ExceptionHandler({RuntimeException.class, UsernameNotFoundException.class})
+    @ExceptionHandler(RuntimeException.class)
     public String handle(){
         return "error";
     }
